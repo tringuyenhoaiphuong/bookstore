3cff6af47579ad8eaac32a0290deed12c16fb213
Canh Le My <lemycanh@gmail.com>
2020-05-12 09:45:08 +0700

    + Initialize

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Detail()
+        {
+            Book book = new Book()
+            {
+                Id = 1,
+                Author = "Apple", 
+                Description = "Long printed dress with thin adjustable straps. V-neckline and wiring under the Dust with ruffles at the bottom of the dress.", 
+                Title = "5 ways to get through your BOOK"
+            };
+            return View(book);
+        }
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double DiscountedPrice { get; set; }
+    }
+}
