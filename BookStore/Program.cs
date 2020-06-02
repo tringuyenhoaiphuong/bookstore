1cf8eb2314fede12d6c76fefaeef2251ea5db0f7
Canh Le My <lemycanh@gmail.com>
2020-06-02 09:25:30 +0700

    * Ready for deployment

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -13,7 +15,14 @@ namespace BookStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = CreateHostBuilder(args).Build();
+            if (args.Contains("--do-migration"))
+            {
+                var dbcontext = (AppDataContext)webHost.Services.GetService(typeof(AppDataContext));
+                dbcontext.Database.Migrate();
+                return;
+            }
+            webHost.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
